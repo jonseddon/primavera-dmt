75c1eca280046c89e11bae4ce044b58f0432edfd
Jon Seddon <jon.seddon@metoffice.gov.uk>
2016-08-09 09:39:19 +0100

    variable query working

--- pdata_app/views.py
+++ pdata_app/views.py
@@ -53,6 +53,7 @@ def view_variable_query_form(request):
 
 
 def view_variable_query_results(request, var_id):
+    # TODO: add some defensive coding to handle no results returned, etc.
     files = DataFile.objects.filter(variable__var_id=var_id)
 
     if not files:
@@ -64,22 +65,29 @@ def view_variable_query_results(request, var_id):
 
     # loop through the unique combinations
     cursor = connection.cursor()
+    variable_id_query = cursor.execute("SELECT id FROM pdata_app_variable WHERE "
+        "var_id=%s", [var_id])
+    variable_id, = variable_id_query.fetchone()
     uniq_rows = cursor.execute('SELECT DISTINCT frequency, climate_model_id, '
-        'experiment_id, project_id FROM pdata_app_datafile WHERE variable_id=%s', [var_id])
-    print uniq_rows
-    print uniq_rows.fetchall()
+        'experiment_id, project_id FROM pdata_app_datafile WHERE '
+        'variable_id=%s', [variable_id])
 
     for row in uniq_rows.fetchall():
+        # unpack the four items from each distinct set of files
         frequency, climate_model, experiment, project = row
+        # find all of the files that contain these distinct items
         row_files = DataFile.objects.filter(variable__var_id=var_id,
             frequency=frequency, climate_model_id=climate_model,
             experiment_id=experiment, project_id=project)
+        # get first file in the set
         first_file = row_files.first()
+        # find the earliest start and latest end times of the set
         start_time = row_files.aggregate(Min('start_time'))['start_time__min']
         end_time = row_files.aggregate(Max('end_time'))['end_time__max']
+        # save the information in a dictionary
         vbles_found.append({
             'project': first_file.project.short_name,
-            'model': first_file.model.short_name,
+            'model': first_file.climate_model.short_name,
             'experiment': first_file.experiment.short_name,
             'frequency': first_file.frequency,
             'start_date': '{:04d}-{:02d}-{:02d}'.format(start_time.year,
@@ -87,7 +95,7 @@ def view_variable_query_results(request, var_id):
             'end_date': '{:04d}-{:02d}-{:02d}'.format(end_time.year,
                 end_time.month, end_time.day)
         })
-
+        # TODO num files, online, ensemble
 
     return render(request, 'variable_query_results.html', {'request': request,
         'page_title': 'Variable Query Results', 'var_id': var_id,
