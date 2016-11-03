d92b925add98eb38440f30ab950aaa47d197eb41
Jon Seddon <jon.seddon@metoffice.gov.uk>
2016-11-03 16:02:20 +0000

    filters added for all components

--- pdata_app/filters.py
+++ pdata_app/filters.py
@@ -4,10 +4,24 @@ from .models import DataRequest
 class DataRequestFilter(django_filters.FilterSet):
     class Meta:
         model = DataRequest
-        fields = ['project', 'experiment']
+        fields = ['project', 'institute', 'climate_model', 'experiment',
+                  'variable_request', 'rip_code']
 
     project = django_filters.CharFilter(name='project__short_name',
                                         lookup_expr='contains')
 
+    institute = django_filters.CharFilter(name='institute__short_name',
+                                          lookup_expr='contains')
+
+    climate_model = django_filters.CharFilter(name='climate_model__short_name',
+                                              lookup_expr='contains')
+
     experiment = django_filters.CharFilter(name='experiment__short_name',
                                            lookup_expr='contains')
+
+    variable_request = django_filters.CharFilter(name='variable_request__cmor_name',
+                                                 lookup_expr='contains')
+
+
+    rip_code = django_filters.CharFilter(name='rip_code',
+                                         lookup_expr='contains')
