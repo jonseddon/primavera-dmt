a7e58a51330cc569c1bfc90e3ae4fe00009f6900
Jon Seddon <jon.seddon@metoffice.gov.uk>
2017-12-22 14:36:52 +0000

    Updated and tested for django-filters v1.1

--- et_indexer/filters.py
+++ et_indexer/filters.py
@@ -8,32 +8,40 @@ class VariableOccurrenceFilter(django_filters.FilterSet):
         model = VariableOccurrence
         fields = ['variable', 'data_file', 'batch_id', 'status', 'directory']
 
-    variable = django_filters.CharFilter(name='variable__var_name',
+    variable = django_filters.CharFilter(field_name='variable__var_name',
                                          lookup_expr='iexact')
 
-    data_file = django_filters.CharFilter(name='data_file__original_location',
-                                          lookup_expr='icontains')
+    data_file = django_filters.CharFilter(
+        field_name='data_file__original_location',
+        lookup_expr='icontains'
+    )
 
-    directory = django_filters.CharFilter(name='data_file__original_location',
-                                          lookup_expr='icontains')
+    directory = django_filters.CharFilter(
+        field_name='data_file__original_location',
+        lookup_expr='icontains'
+    )
 
-    batch_id = django_filters.NumberFilter(name='data_file__batch_id')
+    batch_id = django_filters.NumberFilter(field_name='data_file__batch_id')
 
-    status = django_filters.CharFilter(name='data_file__status',
+    status = django_filters.CharFilter(field_name='data_file__status',
                                        lookup_expr='icontains')
 
 
 class DatafileFilter(django_filters.FilterSet):
     class Meta:
         model = Datafile
+        fields = ['batch_id', 'status', 'original_location', 'directory']
 
-    batch_id = django_filters.NumberFilter(name='batch_id')
 
-    status = django_filters.CharFilter(name='status',
+    batch_id = django_filters.NumberFilter(field_name='batch_id')
+
+    status = django_filters.CharFilter(field_name='status',
                                        lookup_expr='icontains')
 
-    original_location = django_filters.CharFilter(name='original_location',
-                                         lookup_expr='icontains')
+    original_location = django_filters.CharFilter(
+        field_name='original_location',
+        lookup_expr='icontains'
+    )
 
-    directory = django_filters.CharFilter(name='original_location',
+    directory = django_filters.CharFilter(field_name='original_location',
                                          lookup_expr='icontains')
