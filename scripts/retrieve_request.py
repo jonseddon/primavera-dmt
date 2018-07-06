bb36e36135e280100ade4be0595e94f7dab7b3d5
Jon Seddon <jon.seddon@metoffice.gov.uk>
2018-07-06 11:00:39 +0100

    Equivalent change for final check

--- scripts/retrieve_request.py
+++ scripts/retrieve_request.py
@@ -628,8 +628,11 @@ def main(args):
     # check that all files were restored
     offline_files = data_req.datafile_set.filter(online=False)
     missed_timeless_files = offline_files.filter(start_time__isnull=True)
-    missed_data_files = (offline_files.exclude(start_time__isnull=True).
-        filter(start_time__gte=start_float, end_time__lt=end_float))
+    if start_float and end_float:
+        missed_data_files = (offline_files.exclude(start_time__isnull=True).
+            filter(start_time__gte=start_float, end_time__lt=end_float))
+    else:
+        missed_data_files = offline_files.exclude(start_time__isnull=True)
 
     if missed_timeless_files or missed_data_files:
         _email_admin_failure(retrieval)
