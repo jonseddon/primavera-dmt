c459cef3fe64c8082a36058ca7652d61b4e7e28b
Jon Seddon <jon.seddon@metoffice.gov.uk>
2018-07-24 15:12:54 +0100

    Update variant_label on CERFACS AMIP data

---
+++ scripts/update_dreqs/update_dreqs_0076.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+"""
+update_dreqs_0076.py
+
+This file updates all CERFACS AMIP data requests from variant_label
+r1i1p1f1 to r21i1p1f2.
+"""
+import argparse
+import logging.config
+import os
+import sys
+
+import django
+django.setup()
+from pdata_app.models import DataRequest
+
+__version__ = '0.1.0b1'
+
+DEFAULT_LOG_LEVEL = logging.WARNING
+DEFAULT_LOG_FORMAT = '%(levelname)s: %(message)s'
+
+logger = logging.getLogger(__name__)
+
+
+def parse_args():
+    """
+    Parse command-line arguments
+    """
+    parser = argparse.ArgumentParser(description='Add additional data requests')
+    parser.add_argument('-l', '--log-level', help='set logging level to one of '
+        'debug, info, warn (the default), or error')
+    parser.add_argument('--version', action='version',
+        version='%(prog)s {}'.format(__version__))
+    args = parser.parse_args()
+
+    return args
+
+
+def main(args):
+    """
+    Main entry point
+    """
+    orig_amip = DataRequest.objects.filter(
+        institute__short_name='CNRM-CERFACS',
+        experiment__short_name='highresSST-present',
+        rip_code='r1i1p1f1'
+    )
+    logger.debug('{} affected requests found'.format(orig_amip.count()))
+
+    orig_amip.update(rip_code='r21i1p1f2')
+
+
+if __name__ == "__main__":
+    cmd_args = parse_args()
+
+    # determine the log level
+    if cmd_args.log_level:
+        try:
+            log_level = getattr(logging, cmd_args.log_level.upper())
+        except AttributeError:
+            logger.setLevel(logging.WARNING)
+            logger.error('log-level must be one of: debug, info, warn or error')
+            sys.exit(1)
+    else:
+        log_level = DEFAULT_LOG_LEVEL
+
+    # configure the logger
+    logging.config.dictConfig({
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'standard': {
+                'format': DEFAULT_LOG_FORMAT,
+            },
+        },
+        'handlers': {
+            'default': {
+                'level': log_level,
+                'class': 'logging.StreamHandler',
+                'formatter': 'standard'
+            },
+        },
+        'loggers': {
+            '': {
+                'handlers': ['default'],
+                'level': log_level,
+                'propagate': True
+            }
+        }
+    })
+
+    # run the code
+    main(cmd_args)
