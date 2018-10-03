9000f83436a26721597dd88204a96a407029bc60
Jon Seddon <jon.seddon@metoffice.gov.uk>
2018-10-03 13:25:18 +0100

    Retrievals updated

--- scripts/retrieve_request.py
+++ scripts/retrieve_request.py
@@ -286,9 +286,11 @@ def get_et_url(tape_url, data_files, args):
 
     logger.debug('Restoring to {}'.format(retrieval_dir))
 
-    cmd = 'et_get.py -v -l {} -f {} -r {} -t {} 2>&1 | tee -a /group_workspaces/jasmin2/primavera5/.et_logs/stdall.log'.format(
+    cmd = ('/usr/bin/python2.7 /usr/bin/et_get.py -v -l {} -f {} -r {} -t {} '
+           '2>&1 | tee -a /group_workspaces/jasmin2/primavera5/'
+           '.et_logs/stdall.log'.format(
         _make_logfile_name(LOG_FILE_DIR), filelist_name, retrieval_dir,
-        MAX_ET_GET_PROC)
+        MAX_ET_GET_PROC))
 
     logger.debug('et_get.py command is:\n{}'.format(cmd))
 
