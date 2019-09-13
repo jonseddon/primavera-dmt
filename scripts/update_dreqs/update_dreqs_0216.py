2859521b2adad0e04581504fb0f980c208d10445
Jon Seddon <jon.seddon@metoffice.gov.uk>
2019-09-13 10:03:21 +0100

    Remove replaced files

---
+++ scripts/update_dreqs/update_dreqs_0216.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+"""
+update_dreqs_0216.py
+
+Replace 48 files from HadGEM3-GC31-HM control-1950 r1i1p1f1 Amon from 2032
+that were missing the final month.
+"""
+import argparse
+import logging.config
+import sys
+
+import django
+django.setup()
+from pdata_app.utils.replace_file import replace_files
+from pdata_app.models import DataFile
+from pdata_app.utils.common import delete_files
+
+__version__ = '0.1.0b1'
+
+DEFAULT_LOG_LEVEL = logging.WARNING
+DEFAULT_LOG_FORMAT = '%(levelname)s: %(message)s'
+
+logger = logging.getLogger(__name__)
+
+
+def parse_args():
+    """
+    Parse command-line arguments
+    """
+    parser = argparse.ArgumentParser(description='Add additional data requests')
+    parser.add_argument('-l', '--log-level', help='set logging level to one of '
+        'debug, info, warn (the default), or error')
+    parser.add_argument('--version', action='version',
+        version='%(prog)s {}'.format(__version__))
+    args = parser.parse_args()
+
+    return args
+
+
+def main(args):
+    """
+    Main entry point
+    """
+    affected_files = DataFile.objects.filter(
+        climate_model__short_name='HadGEM3-GC31-HM',
+        experiment__short_name='control-1950',
+        rip_code='r1i1p1f1',
+        variable_request__table_name='Amon',
+        name__contains='_203201-'
+    )
+
+    if affected_files.count() != 48:
+        logger.error('{} affected files found'.format(affected_files.count()))
+        sys.exit(1)
+    else:
+        logger.debug('{} affected files found'.format(affected_files.count()))
+
+    delete_files(affected_files)
+    replace_files(affected_files)
+
+
+if __name__ == "__main__":
+    cmd_args = parse_args()
+
+    # determine the log level
+    if cmd_args.log_level:
+        try:
+            log_level = getattr(logging, cmd_args.log_level.upper())
+        except AttributeError:
+            logger.setLevel(logging.WARNING)
+            logger.error('log-level must be one of: debug, info, warn or error')
+            sys.exit(1)
+    else:
+        log_level = DEFAULT_LOG_LEVEL
+
+    # configure the logger
+    logging.config.dictConfig({
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'standard': {
+                'format': DEFAULT_LOG_FORMAT,
+            },
+        },
+        'handlers': {
+            'default': {
+                'level': log_level,
+                'class': 'logging.StreamHandler',
+                'formatter': 'standard'
+            },
+        },
+        'loggers': {
+            '': {
+                'handlers': ['default'],
+                'level': log_level,
+                'propagate': True
+            }
+        }
+    })
+
+    # run the code
+    main(cmd_args)
