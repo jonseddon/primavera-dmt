569f5978e871dbc7eaa7145859421d5e32260e38
Jon Seddon <jon.seddon@metoffice.gov.uk>
2019-09-23 14:15:26 +0100

    Allow calling suite to control file only behaviour.

--- scripts/update_dreqs/update_dreqs_0181.py
+++ scripts/update_dreqs/update_dreqs_0181.py
@@ -34,6 +34,9 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Add additional data requests')
     parser.add_argument('-l', '--log-level', help='set logging level to one of '
         'debug, info, warn (the default), or error')
+    parser.add_argument('-i', '--incoming', help='Update file only, not the '
+                                                 'database.',
+                        action='store_true')
     parser.add_argument('request_id', help='to request id to update')
     parser.add_argument('--version', action='version',
         version='%(prog)s {}'.format(__version__))
@@ -92,7 +95,8 @@ def main(args):
         if created:
             logger.debug('Created {}'.format(new_dreq))
 
-        updater = SourceIdUpdate(data_file, new_source_id)
+        updater = SourceIdUpdate(data_file, new_source_id,
+                                 update_file_only=args.incoming)
         updater.update()
 
 
